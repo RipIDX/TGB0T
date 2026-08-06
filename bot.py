b889825c6c24ca9b94e0b8d1cae218b2767cfc3c
Swagger <ivan2014maligin@gmail.com>
2026-08-06 20:39:03 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,20 +5,21 @@ TOKEN = "8800452125:AAETWvKIeP6BgDKWaSAmAhGm6WAq8TCm7pc"
 ADMIN_ID = 6099860667
 
 bot = telebot.TeleBot(TOKEN)
-app = Flask(__name__)
 
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    if request.headers.get('content-type') == 'application/json':
-        json_string = request.get_data().decode('utf-8')
-        update = telebot.types.Update.de_json(json_string)
-        bot.process_new_updates([update])
-        return '', 200
-    return '', 403
-
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['start', 'help'])
 def start(msg):
     if str(msg.from_user.id) != str(ADMIN_ID):
         bot.reply_to(msg, f"Ваш ID: {msg.from_user.id}")
     else:
-        bot.reply_to(msg, "Бот работает на Render!")
+        bot.reply_to(msg, "Бот на Vercel работает!")
+
+# Vercel serverless function handler
+def handler(request, response):
+    if request.method == 'POST' and request.path == '/api/webhook':
+        json_string = request.body.decode('utf-8')
+        update = telebot.types.Update.de_json(json_string)
+        bot.process_new_updates([update])
+        response.status = 200
+    else:
+        response.status = 404
+    return response
